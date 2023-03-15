3415ca8d5b7f671fec7e493c0f141aa5071e983b
anis520 <ahisahad520@gmail.com>
2023-03-15 22:09:58 +0600

    done

---
+++ api/server.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import colors from "colors"
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+
+
+///init express
+const app=express()
+dotenv.config()
+
+
+/// middlewars
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cors())
+//
+
+ 
+//envroment variable
+const PORT =process.env.PORT || 5050;
+
+
+ 
+
+
+//listen
+app.listen(PORT,()=>{
+
+console.log(`server was runing on port ${PORT}`.bgMagenta.white);
+
+
+
+})
